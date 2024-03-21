007c819dd1d872c4bec6723976f2d9641a700e2d
AmruthaAkhila0730 <152167700+AmruthaAkhila0730@users.noreply.github.com>
2024-03-21 08:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,447 @@
+var fullpics = document.getElementById("fullpics");
+var Fpic = document.getElementById("Fpic");
+
+// Marriage pictures
+function openimgM1(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage1.jpg";
+    }
+    openimgM1();
+function openimgM2(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage2.jpg";
+    }
+    openimgM2();
+function openimgM3(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage3.jpg";
+    }
+    openimgM3();
+function openimgM4(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage4.jpg";
+    }
+    openimgM4();
+function openimgM5(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage5.jpg";
+    }
+    openimgM5();
+function openimgM6(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage6.jpg";
+    }
+    openimgM6();
+function openimgM7(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage7.jpg";
+    }
+    openimgM7();
+function openimgM8(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage8.jpg";
+    }
+    openimgM8();
+function openimgM9(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage9.jpg";
+    }
+    openimgM9();
+function openimgM10(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage10.jpg";
+    }
+    openimgM10();
+function openimgM11(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage11.jpg";
+    }
+    openimgM11();
+function openimgM12(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/marriage12.jpg";
+    }
+    openimgM12();
+
+// Engagement pictures
+function openimgE1(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage1.jpg";
+    }
+    openimgE1();
+function openimgE2(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage2.jpg";
+    }
+    openimgE2();
+function openimgE3(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage3.jpg";
+    }
+    openimgE3();
+function openimgE4(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage4.jpg";
+    }
+    openimgE4();
+function openimgE5(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage5.jpg";
+    }
+    openimgE5();
+function openimgE6(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage6.jpg";
+    }
+    openimgE6();
+function openimgE7(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage7.jpg";
+    }
+    openimgE7();
+function openimgE8(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage8.jpg";
+    }
+    openimgE8();
+function openimgE9(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage9.jpg";
+    }
+    openimgE9();
+function openimgE10(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage10.jpg";
+    }
+    openimgE10();
+function openimgE11(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage11.jpg";
+    }
+    openimgE11();
+function openimgE12(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/engage12.jpg";
+    }
+    openimgE12();
+
+// Birthaday pictures
+function openimgB1(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth1.jpg";
+    }
+    openimgB1();
+function openimgB2(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth2.jpg";
+    }
+    openimgB2();
+function openimgB3(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth3.jpg";
+    }
+    openimgB3();
+function openimgB4(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth4.jpg";
+    }
+    openimgB4();
+function openimgB5(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth5.jpg";
+    }
+    openimgB5();
+function openimgB6(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth6.jpg";
+    }
+    openimgB6();
+function openimgB7(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth7.jpg";
+    }
+    openimgB7();
+function openimgB8(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth8.jpg";
+    }
+    openimgB8();
+function openimgB9(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth9.jpg";
+    }
+    openimgB9();
+function openimgB10(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth10.jpg";
+    }
+    openimgB10();
+function openimgB11(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth11.jpg";
+    }
+    openimgB11();
+function openimgB12(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/birth12.jpg";
+    }
+    openimgB12();
+
+// night parties pictures
+function openimgN1(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night1.jpg";
+    }
+    openimgN1();
+function openimgN2(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night2.jpg";
+    }
+    openimgN2();
+function openimgN3(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night3.jpg";
+    }
+    openimgN3();
+function openimgN4(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night4.jpg";
+    }
+    openimgN4();
+function openimgN5(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night5.jpg";
+    }
+    openimgN5();
+function openimgN6(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night6.jpg";
+    }
+    openimgN6();
+function openimgN7(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night7.jpg";
+    }
+    openimgN7();
+function openimgN8(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night8.jpg";
+    }
+    openimgN8();
+function openimgN9(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night9.jpg";
+    }
+    openimgN9();
+function openimgN10(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night10.jpg";
+    }
+    openimgN10();
+function openimgN11(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night11.jpg";
+    }
+    openimgN11();
+function openimgN12(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/night12.jpg";
+    }
+    openimgN12();
+
+// New born pictures
+function openimgC1(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn1.jpg";
+    }
+    openimgC1();
+function openimgC2(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn2.jpg";
+    }
+    openimgC2();
+function openimgC3(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn3.jpg";
+    }
+    openimgC3();
+function openimgC4(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn4.jpg";
+    }
+    openimgC4();
+function openimgC5(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn5.jpg";
+    }
+    openimgC5();
+function openimgC6(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn6.jpg";
+    }
+    openimgC6();
+function openimgC7(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn7.jpg";
+    }
+    openimgC7();
+function openimgC8(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn8.jpg";
+    }
+    openimgC8();
+function openimgC9(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn9.jpg";
+    }
+    openimgC9();
+function openimgC10(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn10.jpg";
+    }
+    openimgC10();
+function openimgC11(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn11.jpg";
+    }
+    openimgC11();
+function openimgC12(){
+        document.getElementById("fullpics").style.display = "flex";
+        document.getElementById("Fpic").src = "/Images/newborn12.jpg";
+    }
+    openimgC12();
+
+// Child-parties pictures
+function openimgCP1(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children1.jpg";
+}
+openimgCP1();
+function openimgCP2(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children2.jpg";
+}
+openimgCP2();
+function openimgCP3(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children3.jpg";
+}
+openimgCP3();
+function openimgCP4(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children4.jpg";
+}
+openimgCP4();
+function openimgCP5(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children5.jpg";
+}
+openimgCP5();
+function openimgCP6(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children6.jpg";
+}
+openimgCP6();
+function openimgCP7(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children7.jpg";
+}
+openimgCP7();
+function openimgCP8(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children8.jpg";
+}
+openimgCP8();
+function openimgCP9(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children9.jpg";
+}
+openimgCP9();
+function openimgCP10(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children10.jpg";
+}
+openimgCP10();
+function openimgCP11(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children11.jpg";
+}
+openimgCP11();
+function openimgCP12(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/children12.jpg";
+}
+openimgCP12();
+
+// Nature pictures
+function openimgNT1(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature1.jpg";
+}
+openimgNT1();
+function openimgNT2(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature2.jpg";
+}
+openimgNT2();
+function openimgNT3(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature3.jpg";
+}
+openimgNT3();
+function openimgNT4(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature4.jpg";
+}
+openimgNT4();
+function openimgNT5(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature5.jpg";
+}
+openimgNT5();
+function openimgNT6(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature6.jpg";
+}
+openimgNT6();
+function openimgNT7(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature7.jpg";
+}
+openimgNT7();
+function openimgNT8(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature8.jpg";
+}
+openimgNT8();
+function openimgNT9(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature9.jpg";
+}
+openimgNT9();
+function openimgNT10(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature10.jpg";
+}
+openimgNT10();
+function openimgNT11(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature11.jpg";
+}
+openimgNT11();
+function openimgNT12(){
+    document.getElementById("fullpics").style.display = "flex";
+    document.getElementById("Fpic").src = "/Images/nature12.jpg";
+}
+openimgNT12();
+
+//More clicking
+function more(){
+alert("User can't view a picture here...")
+} 
+
+// Closing
+function closeimg(){
+        document.getElementById("fullpics").style.display= "none";
+    }
+    closeimg();
